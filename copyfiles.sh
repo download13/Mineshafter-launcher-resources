d2a4281ded0a077d3b9c48d5cadd204f45bc6f2a
Erin Dachtler <erin@erindachtler.me>
2019-09-05 10:05:30 -0700

    Easier copying to resource directory

---
+++ copyfiles.sh
@@ -0,0 +1,9 @@
+rm ../Mineshafter\ Launcher/resources/*
+
+cp bin/com/mojang/authlib/properties/Property.class ../Mineshafter\ Launcher/resources/
+cp bin/com/mojang/launcher/updater/download/assets/AssetDownloadable.class ../Mineshafter\ Launcher/resources/
+cp bin/com/mojang/launcher/updater/download/assets/AssetDownloadable\$Status.class ../Mineshafter\ Launcher/resources/
+cp bin/net/minecraft/launcher/game/MinecraftGameRunner.class ../Mineshafter\ Launcher/resources/
+cp bin/net/minecraft/launcher/game/MinecraftGameRunner\$1.class ../Mineshafter\ Launcher/resources/
+cp bin/net/minecraft/launcher/updater/CompleteMinecraftVersion.class ../Mineshafter\ Launcher/resources/
+cp bin/net/minecraft/launcher/updater/DownloadType.class ../Mineshafter\ Launcher/resources/
